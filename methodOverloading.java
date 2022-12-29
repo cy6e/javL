f592b02b09ed793a5ad9d3f72e9ec31f202e4d3f
cy6e <97270008+cy6e@users.noreply.github.com>
2022-12-29 22:31:13 +0530

    Update methodOverloading.java

--- methodOverloading.java
+++ methodOverloading.java
@@ -24,3 +24,28 @@ System.out.println(obj.sum(10,10,10));
 System.out.println(obj.sum(10.5,10.5));
 }
 }
+
+//////////////////////////////////////////////////////////////////
+
+import java.util.Scanner;
+class HelperService {
+
+   		 private String formatNumber(int value) {
+       	 return String.format("%d", value);
+    }
+
+   		 private String formatNumber(double value) {
+       	 return String.format("%.3f", value);
+   	 }
+
+  		  private String formatNumber(String value) {
+       	 return String.format("%.2f", Double.parseDouble(value));
+    }
+
+   		 public static void main(String[] args) {
+        		HelperService hs = new HelperService();
+       		 System.out.println(hs.formatNumber(500));
+       		 System.out.println(hs.formatNumber(89.9934));
+       		 System.out.println(hs.formatNumber("550"));
+    }
+}
